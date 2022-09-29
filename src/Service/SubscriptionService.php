a631b814d15a43b5a21777a370a2b06d61db1be8
Joey de Haas <joeydros@msn.com>
2022-09-29 10:28:51 +0200

    update code styling
    Co-authored-by: Reindert <16726304+reindert-vetter@users.noreply.github.com>

--- src/Service/SubscriptionService.php
+++ src/Service/SubscriptionService.php
@@ -72,7 +72,6 @@ class SubscriptionService
         return BodyToSubscription::build($response->body()['data']);
     }
 
-
     public function delete(int $subscriptionId): void
     {
         $this->client->delete("/v2/subscriptions/$subscriptionId");
