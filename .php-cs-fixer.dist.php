a8ac9cf009b911c179b1ff024658c7f1f4e4b791
joeydehaas <joeydros@msn.com>
2022-10-07 14:54:00 +0200

    update cs fixer to align variables

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,7 +7,10 @@ $rules = [
     'array_syntax' => ['syntax' => 'short'],
     'binary_operator_spaces' => [
         'default' => 'single_space',
-        'operators' => ['=>' => null]
+        'operators' => [
+            '=' => 'align',
+            '=>' => 'align',
+        ]
     ],
     'blank_line_after_namespace' => true,
     'blank_line_after_opening_tag' => true,
