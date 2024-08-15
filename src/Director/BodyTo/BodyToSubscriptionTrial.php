e2d75d1468a602fe2c15aedc71650059e8b799d1
Flavio Schoute <flavioschoute@mbp-van-flavio.kpn>
2024-08-15 13:01:40 +0200

    fix tests

--- src/Director/BodyTo/BodyToSubscriptionTrial.php
+++ src/Director/BodyTo/BodyToSubscriptionTrial.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace PlugAndPay\Sdk\Director\BodyTo;
 
+use DateTimeImmutable;
+use Exception;
 use PlugAndPay\Sdk\Contract\BuildObjectInterface;
 use PlugAndPay\Sdk\Entity\SubscriptionTrial;
 use PlugAndPay\Sdk\Exception\DecodeResponseException;
-use PlugAndPay\Sdk\Support\DateUtils;
 
 class BodyToSubscriptionTrial implements BuildObjectInterface
 {
@@ -17,8 +18,23 @@ class BodyToSubscriptionTrial implements BuildObjectInterface
     public static function build(array $data): SubscriptionTrial
     {
         return (new SubscriptionTrial())
-            ->setEndDate(DateUtils::extractDate($data, 'end'))
-            ->setStartDate(DateUtils::extractDate($data, 'start'))
+            ->setEndDate(self::date($data, 'end'))
+            ->setStartDate(self::date($data, 'start'))
             ->setIsActive($data['is_active']);
+    }
+
+    /**
+     * @throws DecodeResponseException
+     * @codeCoverageIgnore
+     */
+    private static function date(array $data, string $field): DateTimeImmutable
+    {
+        try {
+            return new DateTimeImmutable($data[$field]);
+        } catch (Exception $e) {
+            /** @noinspection JsonEncodingApiUsageInspection */
+            $body = (string) json_encode($data, JSON_ERROR_NONE);
+            throw new DecodeResponseException($body, $field, $e);
+        }
     }
 }
