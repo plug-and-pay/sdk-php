e18bb10e0a5c5e53b91e64b9ff02a7a3c774b31e
joeydehaas <joeydros@msn.com>
2024-09-05 15:33:56 +0200

    INTEG-251: add checkout includes

--- src/Service/CheckoutService.php
+++ src/Service/CheckoutService.php
@@ -7,7 +7,9 @@ namespace PlugAndPay\Sdk\Service;
 use PlugAndPay\Sdk\Contract\ClientInterface;
 use PlugAndPay\Sdk\Director\BodyTo\BodyToCheckout;
 use PlugAndPay\Sdk\Entity\Checkout;
+use PlugAndPay\Sdk\Enum\CheckoutIncludes;
 use PlugAndPay\Sdk\Exception\DecodeResponseException;
+use PlugAndPay\Sdk\Support\Parameters;
 
 class CheckoutService
 {
@@ -18,10 +20,18 @@ class CheckoutService
         $this->client = $client;
     }
 
+    public function include(CheckoutIncludes ...$includes): self
+    {
+        $this->includes = $includes;
+
+        return $this;
+    }
+
     /** @throws DecodeResponseException */
     public function find(int $id): Checkout
     {
-        $response = $this->client->get("/v2/checkouts/$id");
+        $query    = Parameters::toString(['include' => $this->includes]);
+        $response = $this->client->get("/v2/checkouts/$id$query");
 
         return BodyToCheckout::build($response->body()['data']);
     }
