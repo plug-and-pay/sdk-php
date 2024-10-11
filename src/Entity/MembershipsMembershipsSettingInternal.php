3c0c22a09f8c583609ac92d1bb574d9ec68de55e
joeydehaas <joeydros@msn.com>
2024-10-11 14:21:23 +0200

    add memberships setting to sdk

---
+++ src/Entity/MembershipsMembershipsSettingInternal.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PlugAndPay\Sdk\Entity;
+
+use DateTimeImmutable;
+
+class MembershipsMembershipsSettingInternal extends MembershipsSetting
+{
+    /**
+     * @internal
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @internal
+     */
+    public function setCreatedAt(DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @internal
+     */
+    public function setUpdatedAt(?DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
