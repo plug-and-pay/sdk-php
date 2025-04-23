aee7ff1f1fb9cbefc03c49b6b75151cbd87f5fcd
joeydehaas <joeydros@msn.com>
2025-04-23 11:20:04 +0200

    fix: remove prefix for authorisation url

--- src/Service/Client.php
+++ src/Service/Client.php
@@ -206,7 +206,7 @@ class Client implements ClientInterface
             'step'                  => 'select_tenant',
         ]);
 
-        return $this->baseUrl . '/v2/auth/oauth/authorize?' . $query;
+        return $this->baseUrl . '/auth/oauth/authorize?' . $query;
     }
 
     /**
